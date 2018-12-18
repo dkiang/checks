927e9ab3a3e6bd159124f9a0f8769e5aeb45bbf6
dkiang <dkiang@punahou.edu>
2018-12-18 10:31:01 -1000

    Added APMagic check

---
+++ APMagic/check50/__init__.py
@@ -0,0 +1,19 @@
+from check50 import *
+
+
+class APMagic(Checks):
+
+	@check()
+	def exists(self):
+		"""APMagic.c exists."""
+		self.require("APMagic.c")
+
+	@check("exists")
+	def compiles(self):
+		"""APMagic.c compiles."""
+		self.spawn("clang -std=c11 -o APMagic APMagic.c -lcs50 -lm").exit(0)
+
+	@check("compiles")
+	def test1(self):
+		"""Checks non-improving scores"""
+		self.spawn("./APMagic").stdin("50 50 20 80 53").stdout("Magic Off: 50.6\n", "Magic Off: 50.6\n").exit(0)
