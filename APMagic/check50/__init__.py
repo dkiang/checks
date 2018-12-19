4c3cb62b0437fd6cbf597af8f8510f3f3c0b8178
dkiang <dkiang@punahou.edu>
2018-12-19 12:51:32 -1000

    Added other checks to APMagic

--- APMagic/check50/__init__.py
+++ APMagic/check50/__init__.py
@@ -16,4 +16,17 @@ class APMagic(Checks):
 	@check("compiles")
 	def test1(self):
 		"""Checks non-improving scores"""
-		self.spawn("./APMagic 50 50 20 80 5").stdout("Magic Off: 50.6\n", "Magic Off: 50.6\n").exit(0)
+		self.spawn("./APMagic 50 50 20 80 53").stdout("Magic Off: 50.6\n", "Magic Off: 50.6\n").exit(0)
+	
+	def test2(self):
+			"""Checks improving scores"""
+			self.spawn("./APMagic 20 50 50 80").stdout("Magic On: 65.0\n", "Magic On: 65.0\n").exit(0)
+			
+	def test3(self):
+			"""Checks improving scores"""
+			self.spawn("./APMagic 1 2 3 4 5 6 7 8").stdout("Magic On: 6.5\n", "Magic On: 6.5\n").exit(0)
+			
+	def test4(self):
+				"""Checks non-improving scores"""
+				self.spawn("./APMagic 1 2 3 4 5 6 8 7").stdout("Magic Off: 6.5\n", "Magic Off: 4.5\n").exit(0)
+		
