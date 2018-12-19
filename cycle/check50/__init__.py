e5711cc5fe32e5bc25c5f0c523c351da3440e879
dkiang <dkiang@punahou.edu>
2018-12-19 13:11:12 -1000

    Added Cycle tests

--- cycle/check50/__init__.py
+++ cycle/check50/__init__.py
@@ -15,5 +15,20 @@ class cycle(Checks):
 
 	@check("compiles")
 	def test1(self):
-		"""identifies numbers greater than 31"""
+		"""identifies numbers less than 31"""
 		self.spawn("./cycle 30 3 B").stdout("B C S S D E F A B S S C D E F A S S B C D E F S S A B C D E \n", "B C S S D E F A B S S C D E F A S S B C D E F S S A B C D E \n").exit(0)
+	
+	@check("compiles")
+	def test2(self):
+			"""identifies numbers greater than 31"""
+			self.spawn("./cycle 33 3 B").exit(1)
+	
+	@check("compiles")
+	def test3(self):
+			"""identifies weeks starting with Saturday"""
+			self.spawn("./cycle 28 1 A").stdout("S S A B C D E S S F A B C D S S E F A B C S S D E F A B \n", "S S A B C D E S S F A B C D S S E F A B C S S D E F A B \n").exit(0)
+
+	@check("compiles")
+	def test4(self):
+			"""identifies weeks where Saturday is the 7th day"""
+			self.spawn("./cycle 31 7 B").stdout("S B C D E F S S A B C D E S S F A B C D S S E F A B C S S D E \n", "S B C D E F S S A B C D E S S F A B C D S S E F A B C S S D E \n").exit(0)
