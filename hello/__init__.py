c076367dbf24d8723008915d6ecde1a6190eb5b8
dkiang <dkiang@punahou.edu>
2019-12-04 10:10:05 -1000

    Fixed typo

--- hello/__init__.py
+++ hello/__init__.py
@@ -6,12 +6,12 @@ def exists():
 	"""hello.c exists."""
 	check50.exists("hello.c")
 
-	@check("exists")
+	@check50.check("exists")
 	def compiles(self):
 		"""hello.c compiles."""
 		check50.c.compile("./hello", lcs50=True)
 
-	@check("compiles")
+	@check50.check("compiles")
 	def test1():
 		"""Checks output"""
 		check50.c.run("./hello").stdin("Douglas").stdout("Hello Douglas\n", "Hello Douglas\n").exit(0)
