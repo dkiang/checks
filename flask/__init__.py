85f960a6ebe707600c0bf176dc695b256839205b
dkiang <douglas.kiang@gmail.com>
2020-04-19 18:51:08 -1000

    Chnaged tabs to spaces

--- flask/__init__.py
+++ flask/__init__.py
@@ -3,15 +3,15 @@ import check50.c
 
 @check50.check()
 def exists():
-	"""application.py exists."""
-	check50.exists("application.py")
-	"""style.css exists."""
-	check50.exists("static/style.css")
-	"""layout.html exists."""
-	check50.exists("templates/layout.html")
-	"""index.html exists."""
-	check50.exists("templates/index.html")
-	"""oops.html exists."""
-	check50.exists("templates/oops.html")
-	"""cross.html exists."""
-	check50.exists("templates/cross.html")
+    """application.py exists."""
+    check50.exists("application.py")
+    """style.css exists."""
+    check50.exists("static/style.css")
+    """layout.html exists."""
+    check50.exists("templates/layout.html")
+    """index.html exists."""
+    check50.exists("templates/index.html")
+    """oops.html exists."""
+    check50.exists("templates/oops.html")
+    """cross.html exists."""
+    check50.exists("templates/cross.html")
