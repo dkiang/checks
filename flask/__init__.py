01dcf260303eef93aeddc8579ed82ea8cc7c9a43
dkiang <douglas.kiang@gmail.com>
2020-04-19 19:03:40 -1000

    Configured exists as an additional check.

--- flask/__init__.py
+++ flask/__init__.py
@@ -2,8 +2,11 @@ import check50
 import check50.c
 
 @check50.check()
-def exists():
+def appexists():
     """application.py exists."""
     check50.exists("application.py")
+
+@check50.check(appexists)
+def styleexists(): 
     """style.css exists."""
     check50.exists("static/style.css")
