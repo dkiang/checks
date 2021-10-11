b0fdb048d8f72d48ef564880653bd9d422ccc2f1
ThinkAlex <ahaddaddelaveau@icloud.com>
2021-10-11 10:15:30 -0700

    Removes need for previous answer to be correct

--- diamonds/__init__.py
+++ diamonds/__init__.py
@@ -22,109 +22,109 @@ def low_value():
     """rejects low value"""
     check50.run("./diamonds").stdin("0").stdout("Size: ")
 
-@check50.check(low_value)
+@check50.check(prompts_user)
 def high_value():
     """rejects high value"""
     check50.run("./diamonds").stdin("21").stdout("Size: ")
 
-@check50.check(high_value)
+@check50.check(prompts_user)
 def one():
     """1 row diamond"""
     print("*")
     print("\*")
     check50.run("./diamonds").stdin("1").stdout("\*")
 
-@check50.check(one)
+@check50.check(prompts_user)
 def two():
     """2 row diamond"""
     check50.run("./diamonds").stdin("2").stdout("\*\n\*")
 
-@check50.check(two)
+@check50.check(prompts_user)
 def three():
     """3 row diamond"""
     check50.run("./diamonds").stdin("3").stdout(" \*\n\* \*\n \*")
 
-@check50.check(three)
+@check50.check(prompts_user)
 def four():
     """4 row diamond"""
     check50.run("./diamonds").stdin("4").stdout(" \*\n\* \*\n\* \*\n \*")
 
-@check50.check(four)
+@check50.check(prompts_user)
 def five():
     """5 row diamond"""
     check50.run("./diamonds").stdin("5").stdout("  \*\n \* \*\n\* \* \*\n \* \*\n  \*")
 
-@check50.check(five)
+@check50.check(prompts_user)
 def six():
     """6 row diamond"""
     check50.run("./diamonds").stdin("6").stdout("  \*\n \* \*\n\* \* \*\n\* \* \*\n \* \*\n  \*")
 
-@check50.check(six)
+@check50.check(prompts_user)
 def seven():
     """7 row diamond"""
     check50.run("./diamonds").stdin("7").stdout("   \*\n  \* \*\n \* \* \*\n\* \* \* \*\n \* \* \*\n  \* \*\n   \*")
 
-@check50.check(seven)
+@check50.check(prompts_user)
 def eight():
     """8 row diamond"""
     check50.run("./diamonds").stdin("8").stdout("   \*\n  \* \*\n \* \* \*\n\* \* \* \*\n\* \* \* \*\n \* \* \*\n  \* \*\n   \*")
 
-@check50.check(eight)
+@check50.check(prompts_user)
 def nine():
     """9 row diamond"""
     check50.run("./diamonds").stdin("9").stdout("    \*\n   \* \*\n  \* \* \*\n \* \* \* \*\n\* \* \* \* \*\n \* \* \* \*\n  \* \* \*\n   \* \*\n    \*")
 
-@check50.check(nine)
+@check50.check(prompts_user)
 def ten():
     """10 row diamond"""
     check50.run("./diamonds").stdin("10").stdout("    \*\n   \* \*\n  \* \* \*\n \* \* \* \*\n\* \* \* \* \*\n\* \* \* \* \*\n \* \* \* \*\n  \* \* \*\n   \* \*\n    \*")
 
-@check50.check(ten)
+@check50.check(prompts_user)
 def eleven():
     """11 row diamond"""
     check50.run("./diamonds").stdin("11").stdout("     \*\n    \* \*\n   \* \* \*\n  \* \* \* \*\n \* \* \* \* \*\n\* \* \* \* \* \*\n \* \* \* \* \*\n  \* \* \* \*\n   \* \* \*\n    \* \*\n     \*")
 
-@check50.check(eleven)
+@check50.check(prompts_user)
 def twelve():
     """12 row diamond"""
     check50.run("./diamonds").stdin("12").stdout("     \*\n    \* \*\n   \* \* \*\n  \* \* \* \*\n \* \* \* \* \*\n\* \* \* \* \* \*\n\* \* \* \* \* \*\n \* \* \* \* \*\n  \* \* \* \*\n   \* \* \*\n    \* \*\n     \*")
 
-@check50.check(twelve)
+@check50.check(prompts_user)
 def thirteen():
     """13 row diamond"""
     check50.run("./diamonds").stdin("13").stdout("      \*\n     \* \*\n    \* \* \*\n   \* \* \* \*\n  \* \* \* \* \*\n \* \* \* \* \* \*\n\* \* \* \* \* \* \*\n \* \* \* \* \* \*\n  \* \* \* \* \*\n   \* \* \* \*\n    \* \* \*\n     \* \*\n      \*")
 
-@check50.check(thirteen)
+@check50.check(prompts_user)
 def fourteen():
     """14 row diamond"""
     check50.run("./diamonds").stdin("14").stdout("      \*\n     \* \*\n    \* \* \*\n   \* \* \* \*\n  \* \* \* \* \*\n \* \* \* \* \* \*\n\* \* \* \* \* \* \*\n\* \* \* \* \* \* \*\n \* \* \* \* \* \*\n  \* \* \* \* \*\n   \* \* \* \*\n    \* \* \*\n     \* \*\n      \*")
 
-@check50.check(fourteen)
+@check50.check(prompts_user)
 def fifteen():
     """15 row diamond"""
     check50.run("./diamonds").stdin("15").stdout("       \*\n      \* \*\n     \* \* \*\n    \* \* \* \*\n   \* \* \* \* \*\n  \* \* \* \* \* \*\n \* \* \* \* \* \* \*\n\* \* \* \* \* \* \* \*\n \* \* \* \* \* \* \*\n  \* \* \* \* \* \*\n   \* \* \* \* \*\n    \* \* \* \*\n     \* \* \*\n      \* \*\n       \*")
 
-@check50.check(fifteen)
+@check50.check(prompts_user)
 def sixteen():
     """16 row diamond"""
     check50.run("./diamonds").stdin("16").stdout("       \*\n      \* \*\n     \* \* \*\n    \* \* \* \*\n   \* \* \* \* \*\n  \* \* \* \* \* \*\n \* \* \* \* \* \* \*\n\* \* \* \* \* \* \* \*\n\* \* \* \* \* \* \* \*\n \* \* \* \* \* \* \*\n  \* \* \* \* \* \*\n   \* \* \* \* \*\n    \* \* \* \*\n     \* \* \*\n      \* \*\n       \*")
 
-@check50.check(sixteen)
+@check50.check(prompts_user)
 def seventeen():
     """17 row diamond"""
     check50.run("./diamonds").stdin("17").stdout("        \*\n       \* \*\n      \* \* \*\n     \* \* \* \*\n    \* \* \* \* \*\n   \* \* \* \* \* \*\n  \* \* \* \* \* \* \*\n \* \* \* \* \* \* \* \*\n\* \* \* \* \* \* \* \* \*\n \* \* \* \* \* \* \* \*\n  \* \* \* \* \* \* \*\n   \* \* \* \* \* \*\n    \* \* \* \* \*\n     \* \* \* \*\n      \* \* \*\n       \* \*\n        \*")
 
-@check50.check(seventeen)
+@check50.check(prompts_user)
 def eighteen():
     """18 row diamond"""
     check50.run("./diamonds").stdin("18").stdout("        \*\n       \* \*\n      \* \* \*\n     \* \* \* \*\n    \* \* \* \* \*\n   \* \* \* \* \* \*\n  \* \* \* \* \* \* \*\n \* \* \* \* \* \* \* \*\n\* \* \* \* \* \* \* \* \*\n\* \* \* \* \* \* \* \* \*\n \* \* \* \* \* \* \* \*\n  \* \* \* \* \* \* \*\n   \* \* \* \* \* \*\n    \* \* \* \* \*\n     \* \* \* \*\n      \* \* \*\n       \* \*\n        \*")
 
-@check50.check(eighteen)
+@check50.check(prompts_user)
 def nineteen():
     """19 row diamond"""
     check50.run("./diamonds").stdin("19").stdout("         \*\n        \* \*\n       \* \* \*\n      \* \* \* \*\n     \* \* \* \* \*\n    \* \* \* \* \* \*\n   \* \* \* \* \* \* \*\n  \* \* \* \* \* \* \* \*\n \* \* \* \* \* \* \* \* \*\n\* \* \* \* \* \* \* \* \* \*\n \* \* \* \* \* \* \* \* \*\n  \* \* \* \* \* \* \* \*\n   \* \* \* \* \* \* \*\n    \* \* \* \* \* \*\n     \* \* \* \* \*\n      \* \* \* \*\n       \* \* \*\n        \* \*\n         \*")
 
-@check50.check(nineteen)
+@check50.check(prompts_user)
 def twenty():
     """20 row diamond"""
     check50.run("./diamonds").stdin("20").stdout("         \*\n        \* \*\n       \* \* \*\n      \* \* \* \*\n     \* \* \* \* \*\n    \* \* \* \* \* \*\n   \* \* \* \* \* \* \*\n  \* \* \* \* \* \* \* \*\n \* \* \* \* \* \* \* \* \*\n\* \* \* \* \* \* \* \* \* \*\n\* \* \* \* \* \* \* \* \* \*\n \* \* \* \* \* \* \* \* \*\n  \* \* \* \* \* \* \* \*\n   \* \* \* \* \* \* \*\n    \* \* \* \* \* \*\n     \* \* \* \* \*\n      \* \* \* \*\n       \* \* \*\n        \* \*\n         \*")
