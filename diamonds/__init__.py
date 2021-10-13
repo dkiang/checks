3f4248340026d84845aefdc0a96b78b78f0e0d04
Alexandre Haddad-Delaveau <ahaddaddelaveau@gmail.com>
2021-10-13 08:38:05 -0700

    did some stuff that might be illegal

--- diamonds/__init__.py
+++ diamonds/__init__.py
@@ -3,13 +3,9 @@ import check50
 from check50 import c
 
 @check50.check()
-def download():
-    """ Downloading Tests """
-    check50.run("wget https://raw.githubusercontent.com/Alexandre2006/checks/master/diamonds/sample.c")
-
-@check50.check()
 def test():
     """Sample test"""
+    check50.run("wget https://raw.githubusercontent.com/Alexandre2006/checks/master/diamonds/sample.c")
     c.compile("sample.c")
     c.compile("diamonds.c")
     output = check50.run("./diamonds").stdin(10).stdout()
