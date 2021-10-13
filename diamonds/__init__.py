92ca308ea21f3efe3c824629737aae5735d717e5
Alexandre Haddad-Delaveau <ahaddaddelaveau@gmail.com>
2021-10-13 10:26:10 -0700

    Test just to make sure it wont accept garbage values

--- diamonds/__init__.py
+++ diamonds/__init__.py
@@ -117,7 +117,8 @@ def one():
     """Does diamonds.c produce a proper 1 tall diamond?"""
     output = check50.run("./diamonds").stdin("1").stdout()
     if get_valid_outputs(1).count(output) != 0:
-        check50.Mismatch("Expected: " + get_valid_outputs(1) + "\n\n Output: " + output)
-        return True
+        pass
     else:
-        return False
+        raise check50.Mismatch(get_valid_outputs(1), output, help="Note that the valid outputs is a list containing "
+                                                                  "the two valid outputs. Only use one when designing"
+                                                                  " your program.")
