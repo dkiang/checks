f86f58dcd415b5d5e82acf01bae8e5e460568054
Alexandre Haddad-Delaveau <ahaddaddelaveau@gmail.com>
2021-10-14 08:15:10 -0700

    I think i just realized why it hasn't been working.

--- diamonds/__init__.py
+++ diamonds/__init__.py
@@ -116,7 +116,7 @@ def low_value():
 @check50.check()
 def high_value():
     """Does diamonds.c reject a high value?"""
-    check50.c.run("./diamonds").stdin("21", prompt=True, timeout=5).stdout("Size: ")
+    check50.c.run("./diamonds").stdin("21", prompt=False).stdout("Size: ")
 
 
 @check50.check()
