ed68b7d1f56f889d40abbd139e8967b55466505f
dkiang <douglas.kiang@gmail.com>
2024-11-21 07:08:37 -0800

    Adjusted indenting

--- only_letters/__init__.py
+++ only_letters/__init__.py
@@ -12,31 +12,31 @@ def compiles():
   check50.c.compile("only_letters.c", lcs50=True)
 
 @check50.check(compiles)
-  def test1(self):
+def test1(self):
     """Checks lack of arguments"""
-  self.spawn("./only_letters").exit(1)
+    self.spawn("./only_letters").exit(1)
   
 @check50.check(compiles)
-  def test2(self):
+def test2(self):
     """Checks too many arguments"""
-  self.spawn("./only_letters hello world").exit(1)
+    self.spawn("./only_letters hello world").exit(1)
   
 @check50.check(compiles)
-  def test3(self):
+def test3(self):
     """Checks for all letters"""
-self.spawn("./only_letters hello").stdout("Those are all letters.\n").exit(0)
+  self.spawn("./only_letters hello").stdout("Those are all letters.\n").exit(0)
 
 @check50.check(compiles)
-  def test4(self):
+def test4(self):
   """Checks for symbols"""
-  self.spawn("./only_letters hel-lo").stdout("Those are not all letters.\n").exit(0)
+    self.spawn("./only_letters hel-lo").stdout("Those are not all letters.\n").exit(0)
 
 @check50.check(compiles)
-  def test5(self):
+def test5(self):
     """Checks for numbers"""
-  self.spawn("./only_letters hello1").stdout("Those are not all letters.\n").exit(0)
+    self.spawn("./only_letters hello1").stdout("Those are not all letters.\n").exit(0)
 
 @check50.check(compiles)
-  def test6(self):
+def test6(self):
     """Checks for single letter"""
-  self.spawn("./only_letters a").stdout("Those are all letters.\n").exit(0)
+    self.spawn("./only_letters a").stdout("Those are all letters.\n").exit(0)
