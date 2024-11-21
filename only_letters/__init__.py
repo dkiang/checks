f11b76683b1acd1c84e5ca6842bbf9ba638572db
dkiang <douglas.kiang@gmail.com>
2024-11-21 06:59:34 -0800

    Test compile

--- only_letters/__init__.py
+++ only_letters/__init__.py
@@ -10,32 +10,32 @@ def compiles():
   """only_letters.c compiles."""
   check50.c.compile("only_letters.c", lcs50=True)
 
-@check50.check("compiles")
-def test1(self):
-  """Checks lack of arguments"""
-  self.spawn("./only_letters").exit(1)
-		
-@check50.check("noargs")
-def test2(self):
-  """Checks too many arguments"""
-  self.spawn("./only_letters hello world").exit(1)
-		
-@check50.check("xsargs")
-def test3(self):
-  """Checks for all letters"""
-  self.spawn("./only_letters hello").stdout("Those are all letters.\n").exit(0)
-	
-@check50.check("letters")
-def test4(self):
-  """Checks for symbols"""
-  self.spawn("./only_letters hel-lo").stdout("Those are not all letters.\n").exit(0)
-
-@check50.check("dash")
-def test5(self):
-  """Checks for numbers"""
-  self.spawn("./only_letters hello1").stdout("Those are not all letters.\n").exit(0)
-
-@check50.check("numbers")
-def test6(self):
-  """Checks for single letter"""
-  self.spawn("./only_letters a").stdout("Those are all letters.\n").exit(0)
+#@check50.check("compiles")
+#def test1(self):
+# """Checks lack of arguments"""
+# self.spawn("./only_letters").exit(1)
+#		
+#@check50.check("noargs")
+#def test2(self):
+# """Checks too many arguments"""
+# self.spawn("./only_letters hello world").exit(1)
+#		
+#@check50.check("xsargs")
+#def test3(self):
+# """Checks for all letters"""
+# self.spawn("./only_letters hello").stdout("Those are all letters.\n").exit(0)
+#	
+#@check50.check("letters")
+#def test4(self):
+# """Checks for symbols"""
+# self.spawn("./only_letters hel-lo").stdout("Those are not all letters.\n").exit(0)
+#
+#@check50.check("dash")
+#def test5(self):
+# """Checks for numbers"""
+# self.spawn("./only_letters hello1").stdout("Those are not all letters.\n").exit(0)
+#
+#@check50.check("numbers")
+#def test6(self):
+# """Checks for single letter"""
+# self.spawn("./only_letters a").stdout("Those are all letters.\n").exit(0)
